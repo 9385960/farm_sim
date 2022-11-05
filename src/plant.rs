d61573b5e51137dc4bbbb11047f2002119aa1e15
JuanPablo2655 <personal@isidrocasiano.com>
2022-11-05 13:34:45 -0400

    feat: plant impl

--- src/plant.rs
+++ src/plant.rs
@@ -1,15 +1,57 @@
-mod Plant;
+use bevy::prelude::*;
 
+#[derive(Component, Debug)]
 struct Plant {
     lifetime: u16,
     plant_type: Type,
     nourishment: f32,
+    tile: Hex,
 }
 
+#[derive(Debug)]
 enum Type {
     Wheat,
     Carrot,
     Beet,
     Potato,
     Berries,
+}
+
+impl Plant {
+    fn new(plant_type: Type, tile: Hex) -> Plant {
+        Plant {
+            lifetime: 0,
+            plant_type: plant_type,
+            nourishment: 0.0,
+            tile: tile,
+        }
+    }
+
+    fn set_nourishment(&mut self, n: f32) {
+        self.nourishment = n;
+    }
+
+    fn set_lifetime(&mut self, l: u16) {
+        self.lifetime = l;
+    }
+
+    fn set_tile(&mut self, t: Hex) {
+        self.tile = t;
+    }
+
+    fn get_nourishment(&self) -> f32 {
+        self.nourishment
+    }
+
+    fn get_lifetime(&self) -> u16 {
+        self.lifetime
+    }
+
+    fn get_tile(&self) -> Hex {
+        self.tile
+    }
+
+    fn get_type(&self) -> Type {
+        self.plant_type
+    }
 }

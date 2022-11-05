4771d16f6389cfbe943ce959dac23bc7771d41b1
Joseph Wang <jgwang@ad.unc.edu>
2022-11-05 13:07:12 -0400

    jpsepj

--- src/hex_map.rs
+++ src/hex_map.rs
@@ -1,6 +1,9 @@
 mod hex_tile;
 
-let mut grid : Vec<[f32;3]> = Vec::new();
+const ROWS : u32 = 5
+const COLMUNS : u32 = 5
+
+let mut grid : Vec<[[f32;COLUMNS];ROWS]> = Vec::new();
 
 fn render() {
 

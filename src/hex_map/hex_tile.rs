18af03a42749463b970b87cf533f4f4fd9ddf12c
Nikolas <n.jauch@ymail.com>
2022-11-05 14:12:12 -0400

    Fixed hex tile bug, where center wasn't added.

--- src/hex_map/hex_tile.rs
+++ src/hex_map/hex_tile.rs
@@ -1,8 +1,20 @@
 use bevy::prelude::*;
 
+use super::vector::vec_addition;
+
 pub const OUTER_RADIUS: f32 = 1.0;
 const SQRT_3_OVER_2: f32 = 0.86602540378443864676;
 pub const INNER_RADIUS: f32 = OUTER_RADIUS * SQRT_3_OVER_2;
+
+const VERTS: [[f32; 3]; 7] = [
+    [0.0, 0.0, OUTER_RADIUS],
+    [INNER_RADIUS, 0.0, 0.5 * OUTER_RADIUS],
+    [INNER_RADIUS, 0.0, -0.5 * OUTER_RADIUS],
+    [0.0, 0.0, -OUTER_RADIUS],
+    [-INNER_RADIUS, 0.0, -0.5 * OUTER_RADIUS],
+    [-INNER_RADIUS, 0.0, 0.5 * OUTER_RADIUS],
+    [0.0, 0.0, OUTER_RADIUS],
+];
 
 const CENTER: [f32; 3] = [0.0, 0.0, 0.0];
 
@@ -71,16 +83,6 @@ impl Hex {
     }
 }
 
-const VERTS: [[f32; 3]; 7] = [
-    [0.0, 0.0, OUTER_RADIUS],
-    [INNER_RADIUS, 0.0, 0.5 * OUTER_RADIUS],
-    [INNER_RADIUS, 0.0, -0.5 * OUTER_RADIUS],
-    [0.0, 0.0, -OUTER_RADIUS],
-    [-INNER_RADIUS, 0.0, -0.5 * OUTER_RADIUS],
-    [-INNER_RADIUS, 0.0, 0.5 * OUTER_RADIUS],
-    [0.0, 0.0, OUTER_RADIUS],
-];
-
 fn get_hex_verts() -> Vec<[f32; 3]> {
     let mut verts = Vec::new();
     for i in 0..6 {
@@ -95,8 +97,8 @@ fn get_hex_verts_c(center: [f32; 3]) -> Vec<[f32; 3]> {
     let mut verts = Vec::new();
     for i in 0..6 {
         verts.push(center);
-        verts.push(VERTS[i]);
-        verts.push(VERTS[i + 1]);
+        verts.push(vec_addition(VERTS[i],center));
+        verts.push(vec_addition(VERTS[i+1],center));
     }
     verts
 }

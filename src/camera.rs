7e1df3ed914649cab1a498ec623cd1553fe3272c
Nikolas <n.jauch@ymail.com>
2022-11-05 20:42:55 -0400

    Implemented ability to get in game click position.

--- src/camera.rs
+++ src/camera.rs
@@ -3,18 +3,21 @@ use std::f32::consts::PI;
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub fn add_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera3dBundle {
-        projection: OrthographicProjection {
-            scale: 3.0,
-            scaling_mode: ScalingMode::FixedVertical(2.0),
-            ..default()
-        }
+    let proj = OrthographicProjection {
+        scale: 7.0,
+        scaling_mode: ScalingMode::FixedVertical(2.0),
+        ..default()
+    };
+
+    let bundle = Camera3dBundle {
+        projection: proj
         .into(),
         transform: Transform{
             translation: Vec3 { x: 0.0, y: 4.0, z: 0.0 },
             rotation: Quat::from_euler(EulerRot::XYZ, -PI/2.0,0.0, 0.0),
-            scale: Vec3 { x: 2.5, y: 2.5, z: 2.5},
+            scale: Vec3 { x: 1.0, y: 1.0, z: 1.0},
         },
         ..Default::default()
-    });
+    };
+    commands.spawn_bundle(bundle);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod camera;
 mod hex_map;
 mod hex_outline;
 mod light;
+mod mouse_to_map;
 
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
@@ -9,6 +10,7 @@ use camera::add_camera;
 use hex_map::add_hex_map;
 use hex_outline::add_outline;
 use light::{add_light, add_light_dir};
+use mouse_to_map::handle_mouse_clicks;
 
 fn main() {
     App::new()
@@ -16,6 +18,7 @@ fn main() {
         .add_startup_system(add_camera)
         .add_startup_system(add_light_dir)
         .add_startup_system(add_hex_map)
+        .add_system(handle_mouse_clicks)
         .add_plugin(WorldInspectorPlugin::new())
         //.add_startup_system(add_outline.after(add_hex_map))
         .run()

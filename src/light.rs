7ae54bcabf372637a595db039cfd4d71e95a507c
JuanPablo2655 <personal@isidrocasiano.com>
2022-11-05 22:26:21 -0400

    chore: format

--- src/light.rs
+++ src/light.rs
@@ -16,10 +16,18 @@ pub fn add_light(mut commands: Commands) {
 
 pub fn add_light_dir(mut commands: Commands) {
     commands.spawn_bundle(DirectionalLightBundle {
-        transform: Transform{
-            translation: Vec3 { x: 0.0, y: 8.0, z: 0.0 },
-            rotation: Quat::from_euler(EulerRot::XYZ, -PI/1.5, 0.0, 0.0),
-            scale:Vec3 { x: 1.0, y: 1.0, z: 1.0 },
+        transform: Transform {
+            translation: Vec3 {
+                x: 0.0,
+                y: 8.0,
+                z: 0.0,
+            },
+            rotation: Quat::from_euler(EulerRot::XYZ, -PI / 1.5, 0.0, 0.0),
+            scale: Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            },
         },
         ..default()
     });

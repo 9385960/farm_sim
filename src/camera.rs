5371a1bb25803200487264e7ee82890eab5c37c8
Nikolas <n.jauch@ymail.com>
2022-11-05 22:17:16 -0400

    Mouse to world deleted

--- src/camera.rs
+++ src/camera.rs
@@ -3,9 +3,9 @@ use std::f32::consts::PI;
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub fn add_camera(mut commands: Commands) {
-    let proj = OrthographicProjection {
-        scale: 7.0,
-        scaling_mode: ScalingMode::FixedVertical(2.0),
+    let proj = PerspectiveProjection {
+        //scale: 7.0,
+        //scaling_mode: ScalingMode::FixedVertical(2.0),
         ..default()
     };
 
@@ -13,7 +13,7 @@ pub fn add_camera(mut commands: Commands) {
         projection: proj
         .into(),
         transform: Transform{
-            translation: Vec3 { x: 0.0, y: 4.0, z: 0.0 },
+            translation: Vec3 { x: 0.0, y: 4.0, z: 2.0 },
             rotation: Quat::from_euler(EulerRot::XYZ, -PI/2.0,0.0, 0.0),
             scale: Vec3 { x: 1.0, y: 1.0, z: 1.0},
         },
